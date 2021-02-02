9c5c03983c865d8a6a8b689e6511640fd4a0e4f1
barry gardiner <G00398792@gmit.ie>
2021-02-02 00:41:54 +0000

    week 2

---
+++ week2/nameAndAge.py
@@ -0,0 +1,12 @@
+# nameAndAge.py
+# This program reads in a number and adds 1 to it
+# author: Barry Gardiner
+
+name = input("Enter your Name:")
+age = input("Enter your Age:")
+
+print('Hello {}\nYour Age is {}'.format(name, age))#using format to fromat name
+
+print(24 - 7)
+
+print(2==3)
